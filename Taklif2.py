b9cab4b5dc9e3f844cdb8fb7df836263f49bf0b8
kaboutari <kaboutari.a@gmail.com>
2024-06-01 12:27:59 +0330

    Update Taklif2.py

--- Taklif2.py
+++ Taklif2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #---------------------------------
 #Taklif2: Load DataSet
 url2="F:\data\weight-height.csv"
-https://gist.github.com/nstokoe/7d4717e96c21b8ad04ec91f361b000cb#file-weight-height-csv
+#https://gist.github.com/nstokoe/7d4717e96c21b8ad04ec91f361b000cb#file-weight-height-csv
 dataset2 = pd.read_csv(url2)
 #---------------------------------
 height=dataset2.Height.array.reshape(-1, 1)
